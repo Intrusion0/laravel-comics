08fd41500e34dbe50891da65a7db126097ccec93
Mario <lombardomario01@gmail.com>
2022-01-21 17:07:53 +0100

    Component Footer completed

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,197 @@
+<footer>
+    <section id="comics-footer">
+        <div class="ms-container">
+            <div class="nav-footer">
+                <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Characters
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                comics
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Movies
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                TV
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Games
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Videos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                News
+                            </a>
+                        </li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Shop DC
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop DC Collectibles
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Terms Of Use
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Privacy policy (New)
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ad Choices
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Avdvertising
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Jobs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Talent Workshops
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                CPSC Certificates
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ratings
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop Help
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SITES</h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                DC
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                MAD Magazine
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Kids
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Universe
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Power Visa
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cookies-settings-text">
+                    All Site Content TM and &copy; 2020 DC Entrertainment, unless otherwise <span>noted here</span>. All rights reserved.
+                    <span>Cookies Settings</span>
+                </div>
+            </div>
+            <div class="logo-footer">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/600px-DC_Comics_logo.png" alt="logo dc">
+            </div>
+        </div>
+    </section>
+    <section id="social-footer">
+        <div class="ms-container">
+            <div class="btn-signup">
+                <a href="#">SIGN-UP NOW!</a>
+            </div>
+            <div class="socials">
+                <span>
+                    FOLLOW US
+                </span>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.uniba.it/ricerca/dipartimenti/farmacia/facebook/image" alt="icon facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png" alt="icon twitter">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="http://nazionale.autonomidipolizia.it/wp-content/uploads/2019/05/youtube.png" class="icon-youtube" alt="icon youtube">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.isnotdown.com/assets/pics/pinterest.png" class="icon-pinterest" alt="icon pinterest">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.iconpacks.net/icons/2/free-periscope-logo-icon-2441-thumb.png" alt="icon periscope">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</footer>
