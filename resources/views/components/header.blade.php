58f5855596037fc62edb3051ffd6d63fc1db33bd
Mario <lombardomario01@gmail.com>
2022-01-21 15:25:14 +0100

    updated structure

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <div class="ms-container">
+        <section id="logo-header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="dc logo">
+        </section>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        CHARACTERS
+                    </a>
+                    <a href="#" class="active">
+                        COMICS
+                    </a>
+                    <a href="#">
+                        MOVIES
+                    </a>
+                    <a href="#">
+                        TV
+                    </a>
+                    <a href="#">
+                        GAMES
+                    </a>
+                    <a href="#">
+                        COLLECTIBLES
+                    </a>
+                    <a href="#">
+                        VIDEOS
+                    </a>
+                    <a href="#">
+                        FANS
+                    </a>
+                    <a href="#">
+                        NEWS
+                    </a>
+                    <a href="#">
+                        SHOP
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
