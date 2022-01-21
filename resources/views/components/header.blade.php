a894f7a9f27814edbd50b5448bdae0ab900667cb
Mario <lombardomario01@gmail.com>
2022-01-21 16:12:47 +0100

    component header completed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,14 @@
 <header>
+    <div class="header-top">
+        <div class="ms-container container-header-top">
+            <span>
+                DC POWER℠ VISA®
+            </span>
+            <span>
+                ADDITIONAL DC SITES ▼
+            </span>
+        </div>
+    </div>
     <div class="ms-container">
         <section id="logo-header">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="dc logo">
@@ -9,35 +19,63 @@
                     <a href="#">
                         CHARACTERS
                     </a>
+                </li>
+                <li>
                     <a href="#" class="active">
                         COMICS
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         MOVIES
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         TV
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         GAMES
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         COLLECTIBLES
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         VIDEOS
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         FANS
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         NEWS
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         SHOP
+                        <span class="arrow-down">▼</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="search">
+                        Search
+                        <span>🔍︎</span>
                     </a>
                 </li>
             </ul>
         </nav>
+    </div>
+    <div class="jumbotron-comics">
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/30d771104652791.5f681e77ac1ac.jpg" alt="jumbotron comics">
     </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('actionComics', function () {
+    return view('pages.action-comics');
+});
